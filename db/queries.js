b78a968e67590bdb3a3377012bf3885446095827
Ibrahim Abdullah <ibrahimabdullah2341@gmail.com>
2025-04-15 15:37:38 +0100

    Finished adding queries.

--- db/queries.js
+++ db/queries.js
@@ -1,1 +1,21 @@
-const db = require("../connection");
+const db = require("./connection");
+
+db.query("SELECT username FROM users;")
+  .then(() => {
+    return db.query("SELECT * FROM articles WHERE topic = 'coding'");
+  })
+  .then(() => {
+    return db.query("SELECT * FROM comments WHERE votes < 0;");
+  })
+  .then(() => {
+    return db.query("SELECT * FROM topics");
+  })
+  .then(() => {
+    return db.query("SELECT * FROM articles WHERE author='grumpy19'");
+  })
+  .then(() => {
+    return db.query("SELECT * FROM comments WHERE votes > 10");
+  })
+  .then((data) => {
+    console.log(data.rows);
+  });
